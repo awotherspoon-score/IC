eaef4ac688544d1d28931a94f88a77ecc512df24
Najaf Ali <ali.najaf@gmail.com>
2009-06-01 15:04:20 +0100

    Changed header image in prospective.php, removed garbage

--- htdocs/index.php
+++ htdocs/index.php
@@ -48,6 +48,12 @@
 		
 			<div id='col-12'>
 			
+				
+				<div class='column' id='col-2'>
+					<a href='http://google.com/'>
+					<h2 class='cufoned-headers'>Welcome</h2></a>
+					<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.</p>
+				</div><!-- /col2 -->
 				<div class='column' id='col-1'>
 					
 					<div id='showcase'>
@@ -64,12 +70,6 @@
 					</div>
 					
 				</div><!-- /col1 -->
-				
-				<div class='column' id='col-2'>
-					<a href='http://google.com/'>
-					<h2 class='cufoned-headers'>Welcome</h2></a>
-					<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.</p>
-				</div><!-- /col2 -->
 				
 			</div><!-- /col-12 -->
 			
--- htdocs/prospective.php
+++ htdocs/prospective.php
@@ -1,9 +1,8 @@
-
 <?php include('inc/doctype.php'); ?>
 
 <html>
 <head>
-<link rel='stylesheet' type='text/css' href='css/default.css' />
+<link rel='stylesheet' type='text/css' href='css/shared.css' />
 <script type='text/javascript' src='js/jquery.js'></script>
 <script type='text/javascript' src='js/font/cufon-yui.js'></script>
 <script type='text/javascript' src='js/font/Sanuk-Black_500.font.js'></script>
@@ -11,5 +10,109 @@
 		Cufon.replace('h2');
 		Cufon.replace('h3');
 </script>
-</head>
 
+</head>
+<body id='homepage'>
+	<div id='wrapper'>
+	<h1>Immanuel College</h1>
+	<div id='header'>
+		<div id='header-top'>	
+		</div>
+		
+		<img id='header-image' alt='We nourish our pupils with an engaging focus on Jewish Tradition' src='img/header-prospective.jpg' />
+                <!--
+		<ul id='pcs-nav'>
+				<li class='has-margin'><a href='#'><img src='img/buttons/home-prospective.jpg' /></a></li>
+				<li class='has-margin'><a href='#'><img src='img/buttons/home-current.jpg' /></a></li>
+				<li><a href='#'><img src='img/buttons/home-staff.jpg' /></a></li>
+		</ul>
+                -->
+		
+		<div id='search-box'>
+			<form action='search.php' method='get'>
+				<input type='text' name='search-key' id='search-key' value='Search' />
+				<input type='submit' name='search' value='' id='search-button' />
+			</form>
+		</div>
+		
+		<div id='quick-links'>
+			<select name='quick-links'>
+				<option>Quick Links</option>
+			</select>
+		</div>
+
+	</div> <!-- /header -->
+	
+	<div id='main'>
+		<p id='margin-spacer'>margin-spacer</p>
+		
+	</div><!-- /main -->
+	<div class='cleardiv'></div>
+	<div id='footer'>
+		<div id='footer-col-123'>
+		
+			<div id='footer-col-12'>
+			
+				<div class='footer-column' id='footer-col-1'>
+                                        <h4>The School</h4>
+                                        <ul>
+                                                <li><a href='#'>About The School</a></li>
+                                                <li><a href='#'>Our Ethos</a></li>
+                                                <li><a href='#'>The Immanuel College</a></li>
+                                                <li><a href='#'>The Sixth Form</a></li>
+                                                <li><a href='#'>Alumni</a></li>
+                                        </ul>
+				</div><!-- /footer-col-1 -->
+				
+				<div class='footer-column' id='footer-col-2'>
+                                        <h4>Joining Us</h4>
+                                        <ul>
+                                                <li><a href='#'>Open Day</a></li>
+                                                <li><a href='#'>Entrance Requirements</a></li>
+                                                <li><a href='#'>Admissions</a></li>
+                                                <li><a href='#'>Fees</a></li>
+                                                <li><a href='#'>Application Forms</a></li>
+                                                <li><a href='#'>Staff</a></li>
+                                        </ul>
+				</div><!-- /footer-col2 -->
+				
+			</div><!-- /footer-col-12 -->
+			
+			<div class='footer-column' id='footer-col-3'>
+                                <h4>Way of Life</h4>
+                                <ul>
+                                        <li><a href='#'>Nurturing Our Pupils</a></li>
+                                        <li><a href='#'>Jewish Life And Learning</a></li>
+                                        <li><a href='#'>Extra Curricular Activities</a></li>
+                                </ul>
+
+			</div><!-- /footer-col-3 -->
+			
+		</div><!-- /footer-col-123 -->
+		
+		<div class='footer-column' id='footer-col-4'>
+                        <ul>
+                                <li><a href='#'>News</a></li>
+                                <li><a href='#'>Events</a></li>
+                                <li><a href='#'>Gallery</a></li>
+                                <li><a href='#'>Contact Us</a></li>
+                        </ul>
+		</div><!-- /footer-col-4 -->
+	</div><!-- /footer -->
+        <div id='actual-footer'>
+                <div id='footer-nav'>
+                        <a href='#'>Contact</a> 
+                        <a href='#'>Sitemap</a> 
+                        <a href='#'>Policies</a> 
+                        <a href='#'>Links</a> 
+                </div>
+
+                <div id='footer-copyright'>
+                        Copyright &copy <?= date('Y'); ?> Immanuel College
+                        <a href='#'>Website By <img src='img/icons/score-logo.jpg' />Scorecomms</a>
+                </div>
+        </div>
+	</div><!-- /wrapper -->
+</body>
+
+</html>
