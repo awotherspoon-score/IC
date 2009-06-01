740663c7de3dc83d5f49a7509b4882752baba4e4
Najaf Ali <ali.najaf@gmail.com>
2009-06-01 13:58:06 +0100

    Pointed index.php stylesheet link to home.css

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,7 +2,7 @@
 
 <html>
 <head>
-<link rel='stylesheet' type='text/css' href='css/style.css' />
+<link rel='stylesheet' type='text/css' href='css/home.css' />
 <script type='text/javascript' src='js/jquery.js'></script>
 <script type='text/javascript' src='js/font/cufon-yui.js'></script>
 <script type='text/javascript' src='js/font/Sanuk-Black_500.font.js'></script>
