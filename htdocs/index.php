7cbb9a1986a483da56287e0c1c642bb9771f008e
Najaf Ali <ali.najaf@gmail.com>
2009-06-01 14:18:52 +0100

    created separate 'shared' css file, pointed index to it

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,6 +2,7 @@
 
 <html>
 <head>
+<link rel='stylesheet' type='text/css' href='css/shared.css' />
 <link rel='stylesheet' type='text/css' href='css/home.css' />
 <script type='text/javascript' src='js/jquery.js'></script>
 <script type='text/javascript' src='js/font/cufon-yui.js'></script>
