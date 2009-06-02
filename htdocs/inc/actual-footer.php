b4e67a31d7a87297c4c5bf4b05256f9cab447ff5
Najaf Ali <ali.najaf@gmail.com>
2009-06-02 10:47:09 +0100

    Pulled footers out into include files

---
+++ htdocs/inc/actual-footer.php
@@ -0,0 +1,13 @@
+        <div id='actual-footer'>
+                <div id='footer-nav'>
+                        <a href='#'>Contact</a> 
+                        <a href='#'>Sitemap</a> 
+                        <a href='#'>Policies</a> 
+                        <a href='#'>Links</a> 
+                </div>
+
+                <div id='footer-copyright'>
+                        Copyright &copy <?= date('Y'); ?> Immanuel College
+                        <a href='#'>Website By <img src='img/icons/score-logo.jpg' />Scorecomms</a>
+                </div>
+        </div><!-- /actual-footer -->
--- htdocs/index.php
+++ htdocs/index.php
@@ -102,70 +102,10 @@
 		
 	</div><!-- /main -->
 	<div class='cleardiv'></div>
-	<div id='footer'>
-		<div id='footer-col-123'>
-		
-			<div id='footer-col-12'>
-			
-				<div class='footer-column' id='footer-col-1'>
-                                        <h4>The School</h4>
-                                        <ul>
-                                                <li><a href='#'>About The School</a></li>
-                                                <li><a href='#'>Our Ethos</a></li>
-                                                <li><a href='#'>The Immanuel College</a></li>
-                                                <li><a href='#'>The Sixth Form</a></li>
-                                                <li><a href='#'>Alumni</a></li>
-                                        </ul>
-				</div><!-- /footer-col-1 -->
-				
-				<div class='footer-column' id='footer-col-2'>
-                                        <h4>Joining Us</h4>
-                                        <ul>
-                                                <li><a href='#'>Open Day</a></li>
-                                                <li><a href='#'>Entrance Requirements</a></li>
-                                                <li><a href='#'>Admissions</a></li>
-                                                <li><a href='#'>Fees</a></li>
-                                                <li><a href='#'>Application Forms</a></li>
-                                                <li><a href='#'>Staff</a></li>
-                                        </ul>
-				</div><!-- /footer-col2 -->
-				
-			</div><!-- /footer-col-12 -->
-			
-			<div class='footer-column' id='footer-col-3'>
-                                <h4>Way of Life</h4>
-                                <ul>
-                                        <li><a href='#'>Nurturing Our Pupils</a></li>
-                                        <li><a href='#'>Jewish Life And Learning</a></li>
-                                        <li><a href='#'>Extra Curricular Activities</a></li>
-                                </ul>
 
-			</div><!-- /footer-col-3 -->
-			
-		</div><!-- /footer-col-123 -->
-		
-		<div class='footer-column' id='footer-col-4'>
-                        <ul>
-                                <li><a href='#'>News</a></li>
-                                <li><a href='#'>Events</a></li>
-                                <li><a href='#'>Gallery</a></li>
-                                <li><a href='#'>Contact Us</a></li>
-                        </ul>
-		</div><!-- /footer-col-4 -->
-	</div><!-- /footer -->
-        <div id='actual-footer'>
-                <div id='footer-nav'>
-                        <a href='#'>Contact</a> 
-                        <a href='#'>Sitemap</a> 
-                        <a href='#'>Policies</a> 
-                        <a href='#'>Links</a> 
-                </div>
-
-                <div id='footer-copyright'>
-                        Copyright &copy <?= date('Y'); ?> Immanuel College
-                        <a href='#'>Website By <img src='img/icons/score-logo.jpg' />Scorecomms</a>
-                </div>
-        </div>
+        <?php include('inc/footer.php'); ?>
+        <?php include('inc/actual-footer.php'); ?>
+        
 	</div><!-- /wrapper -->
 </body>
 
