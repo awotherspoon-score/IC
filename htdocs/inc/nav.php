ac6cb1955042804272936d1d65e9fe234df820ae
Najaf Ali <ali.najaf@gmail.com>
2009-06-03 13:51:19 +0100

    Pulled nav out into an inc/nav.php

---
+++ htdocs/inc/nav.php
@@ -0,0 +1,36 @@
+              <div id='top-nav'>
+                        <ul id='top-nav-list'>
+                                <li class='page-nav-link'><a href='#'>The School</a>
+                                        <ul>
+                                                <li><a href='#'>About The School</a></li>
+                                                <li><a href='#'>Our Ethos</a></li>
+                                                <li><a href='#'>The Immanuel Curriculum</a></li>
+                                                <li><a href='#'>The Sixth Form</a></li>
+                                                <li><a href='#'>Alumni</a></li>
+                                        </ul>
+                                </li>
+                                <li class='page-nav-link'><a href='#'>Joining Us</a>
+                                        <ul>
+                                                <li><a href='#'>Open Day</a></li>
+                                                <li><a href='#'>Entrance Requirements</a></li>
+                                                <li><a href='#'>Admissions</a></li>
+                                                <li><a href='#'>Fees</a></li>
+                                                <li><a href='#'>Application Forms</a></li>
+                                                <li><a href='#'>Staff</a></li>
+                                        </ul>
+                                </li>
+                                <li class='page-nav-link'><a href='#'>Way of Life</a>
+                                        <ul>
+                                                <li><a href='#'>Nurturing Our Pupils</a></li>
+                                                <li><a href='#'>Jewish Life and Learning</a></li>
+                                                <li><a href='#'>Extra Curricular Activities</a></li>
+                                        </ul>
+                                </li>
+                                <li class='nav-link'><a href='#'>News</a>
+                                </li>
+                                <li class='nav-link'><a href='#'>Events</a>
+                                </li>
+                                <li class='nav-link'><a href='#'/>Gallery</a></li>
+                                <li class='nav-link'><a href='#'/>Contact Us</a></li>
+                        </ul>
+                </div><!-- /top-nav -->
