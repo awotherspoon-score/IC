184d8c3095d75c4fc65381fe93a8a2663cd26079
Najaf Ali <ali.najaf@gmail.com>
2009-06-03 13:40:31 +0100

    Added 2nd level nav html, no styling/scripting

--- htdocs/prospective.php
+++ htdocs/prospective.php
@@ -12,7 +12,8 @@
 <script type="text/javascript">
 		Cufon.replace('h2', { fontFamily: 'Sanuk-Black'});
 		Cufon.replace('h3', { fontFamily: 'Sanuk-Black'});
-		Cufon.replace('#top-nav li', { fontFamily: 'Sanuk-Black'});
+		Cufon.replace('li.page-nav-link>a', { fontFamily: 'Sanuk-Black'});
+		Cufon.replace('li.nav-link>a', { fontFamily: 'Sanuk-Black'});
 
 </script>
 
@@ -40,14 +41,39 @@
 			</select>
 		</div>
                 <div id='top-nav'>
-                        <ul>
-                                <li class='page-nav-link'>The School</li>
-                                <li class='page-nav-link'>Joining Us</li>
-                                <li class='page-nav-link'>Way of Life</li>
-                                <li class='nav-link'>News</li>
-                                <li class='nav-link'>Events</li>
-                                <li class='nav-link'>Gallery</li>
-                                <li class='nav-link'>Contact Us</li>
+                        <ul id='top-nav-list'>
+                                <li class='page-nav-link'><a href='#'>The School</a>
+                                        <ul>
+                                                <li><a href='#'>About The School</a></li>
+                                                <li><a href='#'>Our Ethos</a></li>
+                                                <li><a href='#'>The Immanuel Curriculum</a></li>
+                                                <li><a href='#'>The Sixth Form</a></li>
+                                                <li><a href='#'>Alumni</a></li>
+                                        </ul>
+                                </li>
+                                <li class='page-nav-link'><a href='#'>Joining Us</a>
+                                        <ul>
+                                                <li><a href='#'>Open Day</a></li>
+                                                <li><a href='#'>Entrance Requirements</a></li>
+                                                <li><a href='#'>Admissions</a></li>
+                                                <li><a href='#'>Fees</a></li>
+                                                <li><a href='#'>Application Forms</a></li>
+                                                <li><a href='#'>Staff</a></li>
+                                        </ul>
+                                </li>
+                                <li class='page-nav-link'><a href='#'>Way of Life</a>
+                                        <ul>
+                                                <li><a href='#'>Nurturing Our Pupils</a></li>
+                                                <li><a href='#'>Jewish Life and Learning</a></li>
+                                                <li><a href='#'>Extra Curricular Activities</a></li>
+                                        </ul>
+                                </li>
+                                <li class='nav-link'><a href='#'>News</a>
+                                </li>
+                                <li class='nav-link'><a href='#'>Events</a>
+                                </li>
+                                <li class='nav-link'><a href='#'/>Gallery</a></li>
+                                <li class='nav-link'><a href='#'/>Contact Us</a></li>
                         </ul>
                 </div><!-- /top-nav -->
 	</div> <!-- /header -->
