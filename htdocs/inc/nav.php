8c363113b1acf7e14c6f1b3e9e33bf5d04c995b7
Najaf Ali <ali.najaf@gmail.com>
2009-06-04 10:53:29 +0100

    Dropdowns working in IE6! Need hover state for other four links

--- htdocs/inc/nav.php
+++ htdocs/inc/nav.php
@@ -1,6 +1,6 @@
               <div id='top-nav'>
                         <ul id='top-nav-list'>
-                                <li class='page-nav-link'><a href='#'>The School</a>
+                                <li id='the-school-link' class='page-nav-link'><a href='#'>The School</a>
                                         <ul>
                                                 <li><a href='#'>About The School</a></li>
                                                 <li><a href='#'>Our Ethos</a></li>
@@ -9,7 +9,7 @@
                                                 <li><a href='#'>Alumni</a></li>
                                         </ul>
                                 </li>
-                                <li class='page-nav-link'><a href='#'>Joining Us</a>
+                                <li id='joining-us-link' class='page-nav-link'><a href='#'>Joining Us</a>
                                         <ul>
                                                 <li><a href='#'>Open Day</a></li>
                                                 <li><a href='#'>Entrance Requirements</a></li>
@@ -19,18 +19,18 @@
                                                 <li><a href='#'>Staff</a></li>
                                         </ul>
                                 </li>
-                                <li class='page-nav-link'><a href='#'>Way of Life</a>
+                                <li id='way-of-life-link' class='page-nav-link'><a href='#'>Way of Life</a>
                                         <ul>
                                                 <li><a href='#'>Nurturing Our Pupils</a></li>
                                                 <li><a href='#'>Jewish Life and Learning</a></li>
                                                 <li><a href='#'>Extra Curricular Activities</a></li>
                                         </ul>
                                 </li>
-                                <li class='nav-link'><a href='#'>News</a>
+                                <li id='news-link' class='nav-link'><a href='#'>News</a>
                                 </li>
-                                <li class='nav-link'><a href='#'>Events</a>
+                                <li id='events-link' class='nav-link'><a href='#'>Events</a>
                                 </li>
-                                <li class='nav-link'><a href='#'>Gallery</a></li>
-                                <li class='nav-link'><a href='#'>Contact Us</a></li>
+                                <li id='gallery-link' class='nav-link'><a href='#'>Gallery</a></li>
+                                <li id='contact-us-link' class='nav-link'><a href='#'>Contact Us</a></li>
                         </ul>
                 </div><!-- /top-nav -->
