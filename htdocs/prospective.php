9d77af443ea6e8d1fc2d8e1cfa4b9aa99502cb5f
Najaf Ali <ali.najaf@gmail.com>
2009-06-09 15:49:57 +0100

    a very rough first draft of the prospective page template complete

--- htdocs/prospective.php
+++ htdocs/prospective.php
@@ -13,12 +13,11 @@
 <script type='text/javascript' src='js/font/Sanuk-Regular_500.font.js'></script>
 <script type="text/javascript">
 		Cufon.replace('h1', { fontFamily: 'Sanuk-Black'});
-		Cufon.replace('h2', { fontFamily: 'Sanuk-Black'});
-		Cufon.replace('h3', { fontFamily: 'Sanuk-Black'});
 		Cufon.replace('li.page-nav-link>a', { fontFamily: 'Sanuk-Black'});
 		Cufon.replace('li.nav-link>a', { fontFamily: 'Sanuk-Black'});
                 Cufon.replace('li ul li', { fontFamily: 'Sanuk-Regular'});
                 Cufon.replace("#tooltip *", { fontFamily: 'Sanuk-Black'});
+                Cufon.replace("body div#main div#sidebar div#sidebar-links-title p", { fontFamily: 'Sanuk-Black'});
                 
                 $(document).ready(function() {
                   var refreshed = false;
@@ -100,15 +99,29 @@
                         <div id='breadcrumb'>
                         </div>
                         <h1>Welcome Prospective Pupils and Parents</h1>
-<p id='first-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu sapien nisi, eu mollis nisl. Suspendisse id diam mauris, et rutrum tortor. Sed semper, turpis id tempus dictum, ipsum arcu facilisis odio, id vestibulum quam libero ac erat. Duis pharetra, sem vel sagittis sollicitudin, velit eros cursus diam, non convallis odio elit vel sem. Sed dictum est quis nibh mollis sit amet hendrerit ante fermentum. </p>
+                        <p id='first-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu sapien nisi, eu mollis nisl. Suspendisse id diam mauris, et rutrum tortor. Sed semper, turpis id tempus dictum, ipsum arcu facilisis odio, id vestibulum quam libero ac erat. Duis pharetra, sem vel sagittis sollicitudin, velit eros cursus diam, non convallis odio elit vel sem. Sed dictum est quis nibh mollis sit amet hendrerit ante fermentum. </p>
 
-<p>Vestibulum aliquet purus lorem, sit amet volutpat ante. Nulla interdum massa nec lacus ornare gravida. Praesent vitae tortor ut ligula adipiscing <a href='#'>condimentum</a>. Ut dapibus lobortis scelerisque. Suspendisse id felis lacus. Maecenas gravida, quam sed faucibus convallis, felis arcu faucibus lorem, a interdum magna eros eu metus. Cras vitae tellus metus. In pretium pellentesque tortor vel pharetra. Morbi sit amet leo volutpat dui iaculis mattis. Phasellus nec ipsum nisl. Maecenas nec <a href='#'>sapien sapien</a>, quis commodo arcu. Donec odio mi, venenatis quis gravida ut, ultrices sed magna. Pellentesque vestibulum orci eget urna dapibus non pellentesque lacus mollis.</p>
-
-<p>Nulla facilisi. Vivamus ut hendrerit libero. Donec dictum libero non nibh dictum vestibulum. Etiam ut mauris ipsum, sit amet consequat erat. Integer nisi metus, lobortis vel luctus a, tincidunt vel lectus. Sed justo turpis, consectetur ac tincidunt et, consectetur eget eros. Nunc bibendum aliquam ipsum eu feugiat. Etiam mattis faucibus orci eu varius. Nunc auctor scelerisque mi, porttitor rhoncus dui iaculis non. In hac habitasse platea dictumst. Praesent massa velit, mattis id condimentum nec, pellentesque et nulla. Donec egestas mattis varius. Suspendisse consectetur elit eu velit facilisis aliquam. In pretium nunc vitae urna posuere elementum. Duis ut turpis id tortor imperdiet pharetra. Maecenas eget libero nulla, vitae sagittis purus.</p>
+                        <p>Vestibulum aliquet purus lorem, sit amet volutpat ante. Nulla interdum massa nec lacus ornare gravida. Praesent vitae tortor ut ligula adipiscing <a href='#'>condimentum</a>. Ut dapibus lobortis scelerisque. Suspendisse id felis lacus. Maecenas gravida, quam sed faucibus convallis, felis arcu faucibus lorem, a interdum magna eros eu metus. Cras vitae tellus metus. In pretium pellentesque tortor vel pharetra. Morbi sit amet leo volutpat dui iaculis mattis. Phasellus nec ipsum nisl. Maecenas nec <a href='#'>sapien sapien</a>, quis commodo arcu. Donec odio mi, venenatis quis gravida ut, ultrices sed magna. Pellentesque vestibulum orci eget urna dapibus non pellentesque lacus mollis.</p>
+                        <h2>A H2 Heading</h2>
+                        <p>Nulla facilisi. Vivamus ut hendrerit libero. Donec dictum libero non nibh dictum vestibulum. Etiam ut mauris ipsum, sit amet consequat erat. Integer nisi metus, lobortis vel luctus a, tincidunt vel lectus. Sed justo turpis, consectetur ac tincidunt et, consectetur eget eros. </p>
+                        <h3>A h3 heading</h3>
+                        <p>Nunc bibendum aliquam ipsum eu feugiat. Etiam mattis faucibus orci eu varius. Nunc auctor scelerisque mi, porttitor rhoncus dui iaculis non. In hac habitasse platea dictumst. Praesent massa velit, mattis id condimentum nec, pellentesque et nulla. Donec egestas mattis varius. Suspendisse consectetur elit eu velit facilisis aliquam. In pretium nunc vitae urna posuere elementum. Duis ut turpis id tortor imperdiet pharetra. Maecenas eget libero nulla, vitae sagittis purus.</p>
 
                 </div>
                 <div id='sidebar'>
-<p>Nulla facilisi. Vivamus ut hendrerit libero. Donec dictum libero non nibh dictum vestibulum. Etiam ut mauris ipsum, sit amet consequat erat. Integer nisi metus, lobortis vel luctus a, tincidunt vel lectus. Sed justo turpis, consectetur ac tincidunt et, consectetur eget eros. Nunc bibendum aliquam ipsum eu feugiat. Etiam mattis faucibus orci eu varius. Nunc auctor scelerisque mi, porttitor rhoncus dui iaculis non. In hac habitasse platea dictumst. Praesent massa velit, mattis id condimentum nec, pellentesque et nulla. Donec egestas mattis varius. Suspendisse consectetur elit eu velit facilisis aliquam. In pretium nunc vitae urna posuere elementum. Duis ut turpis id tortor imperdiet pharetra. Maecenas eget libero nulla, vitae sagittis purus.</p>
+                <img src='img/post-image.jpg' id='post-image' />
+                <div id='sidebar-links-title'>
+                  <p>Suggested Links</p>
+                </div>
+                <ul id='sidebar-links-list'>
+                        <li><a href='#'>Open Day</a></li>
+                        <li><a href='#'>Entrance Requirements</a></li>
+                        <li><a href='#'>Admissions</a></li>
+                        <li><a href='#'>Fees</a></li>
+                        <li><a href='#'>Nurturing our Pupils</a></li>
+                        <li><a href='#'>Jewish Life and Learning</a></li>
+                </ul>
+
 
                 </div>
 		
