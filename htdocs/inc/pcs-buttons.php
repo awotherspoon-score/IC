a54d82f17a1a0fbbf2276e3de858ec0615f1b469
Najaf Ali <ali.najaf@gmail.com>
2009-07-05 21:04:26 +0100

    final commit

---
+++ htdocs/inc/pcs-buttons.php
@@ -0,0 +1,19 @@
+                <div id='section-select'>
+                        <ul>
+                                <li id='prospective-link'>
+                                        <a href='/prospective.php'>
+                                                <img title='Prospective Students' src='/img/buttons/prospective-button.gif' />
+                                        </a>
+                                </li>
+                                <li id='current-link' class='selected'>
+                                        <a href='/current.php'>
+                                                <img title='Current Students' src='/img/buttons/current-button.gif' />
+                                        </a>
+                                </li>
+                                <li id='staff-link'>
+                                        <a href='/staff.php'>
+                                                 <img title='Staff' src='/img/buttons/staff-button.gif' />
+                                        </a>
+                                </li>
+                        </ul>
+                </div>
--- htdocs/page.php
+++ htdocs/page.php
@@ -95,25 +95,7 @@
 			</select>
 		</div>
                 <?php include('inc/nav.php'); ?>
-                <div id='section-select'>
-                        <ul>
-                                <li id='prospective-link'>
-                                        <a href='prospective.php'>
-                                                <img title='Prospective Students' src='/img/buttons/prospective-button.gif' />
-                                        </a>
-                                </li>
-                                <li id='current-link' class='selected'>
-                                        <a href='current.php'>
-                                                <img title='Current Students' src='/img/buttons/current-button.gif' />
-                                        </a>
-                                </li>
-                                <li id='staff-link'>
-                                        <a href='staff.php'>
-                                                 <img title='Staff' src='/img/buttons/staff-button.gif' />
-                                        </a>
-                                </li>
-                        </ul>
-                </div>
+		<?php include('inc/pcs-buttons.php'); ?>
 	</div> <!-- /header -->
 	
 	<div id='main'>
