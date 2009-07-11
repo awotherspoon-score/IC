bcb0b67f6a799e6d7c80ead9eec445f1209ce693
Najaf Ali <ali.najaf@gmail.com>
2009-07-11 21:19:40 +0100

    added some documentation for commands

--- htdocs/php/command/command.php
+++ htdocs/php/command/command.php
@@ -2,7 +2,15 @@
         /**
          * Command Superclass
          *
-         * Defines one basic function common to all classes
+         * Defines one basic execute function common to all command classes
+	 * Commands act as an interface between the website and the database
+	 * All access (create, retrieve, update, delete) should, in theory, go via these commands
+	 * 
+	 * This gives the backend system a single point of access, so we don't necessarily have to
+	 * be coming from a php website. 
+	 * You could for example hit these commands from ajax (and we do that using 'ajaxcommandrunner.php') or
+	 * an iphone app or anything that can do HTTP
+	 *
          */
         abstract class Command {
                 /**
